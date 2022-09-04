1784daf549464aa2640a3c9ac7c6b2fa8527b4f5
Coreen Huang <coreen.huang@gmail.com>
2022-09-04 07:11:43 -0400

    refactored in put setup into its own file, input.js

--- play.js
+++ play.js
@@ -1,32 +1,14 @@
 const net = require("net");
-const {connect} = require('./client');
-let conn = connect();
 
-const handleUserInput = function (data) {
-  if (data === 'w') {
-    conn.write('Move: up');
-  } else if (data === 's') {
-    conn.write('Move: down');
-  } else if (data === 'a') {
-    conn.write('Move: left');
-  } else if (data === 'd') {
-    conn.write('Move: right');
-  } else if (data === '\u0003') {
-    process.exit();
-  }
-};
+// const {connect} = require("./client");
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 
+// connect();
+
 setupInput();
+
+// both connect lines above commented out as it works fine without them
+// when connect lines are not commented out, there are two snakes that appear in the server
